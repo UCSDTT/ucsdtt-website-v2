7cae74f80fa5ecdff4b7d1be2e2dd286db6df2d4
willxu1234 <wlxu@ucsd.edu>
2019-06-30 12:19:49 -0400

    updated deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
-
-# Use: $ git gh-deploy ./__sapper__/export
+git add -A
+git commit -a
+npm run export
+git push
